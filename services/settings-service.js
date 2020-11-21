30c3c6aa86decef29240c19c1ad23b4451159a52
Sven Piller <sven.piller@gmail.de>
2020-11-21 19:55:21 +0100

    Added SettingsService

--- services/settings-service.js
+++ services/settings-service.js
@@ -1,14 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+
+const settingsFilePath = path.join(__dirname, "../json/settings.json")
+
 function getSettings() {
-  return {}
+  const settingsData = fs.readFileSync(settingsFilePath)
+  return JSON.parse(settingsData);
 }
 
 function writeSettings(newSettings) {
+  const settingsJSON = JSON.stringify(newSettings, null, 2)
+  try {
+    fs.writeFileSync(settingsFilePath, settingsJSON);
+    return true
+  } catch (error) {
+    return false
+  }
 }
 
 function getDefaultDir() {
+  const defaultDir = getSettings().defaultDir;
+  if (!defaultDir) {
+    return process.cwd();
+  }
+  return isValidDir(defaultDir) ? defaultDir : process.cwd();
 }
 
 function isValidDir(dirPath) {
+  try {
+    fs.readdirSync(dirPath)
+    return true
+  } catch (error) {
+    return false
+  }
 }
 
 module.exports = {
